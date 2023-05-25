bcb5330980a72d3a8eeeabd5d12a043c22ada0dd
alanaadams <alananadams@gmail.com>
2023-05-25 18:34:00 -0400

    precipitation dictionary added to app

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,27 +1,35 @@
 # Import the dependencies.
-
-
+from flask import Flask, jsonify
+import numpy as np
+import pandas as pd
+import datetime as dt
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
 #################################################
 # Database Setup
 #################################################
-
+# create engine to hawaii.sqlite
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
+Base = automap_base()
 
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-from flask import Flask
 app = Flask(__name__)
 
 
@@ -43,9 +51,26 @@ def home():
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    """Return the precipitation data as json"""
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
 
-    return jsonify(precipitation_data)
+    """Return a  dictionary of all precipitation data"""
+    # Query all passengers
+    results = session.query(Messenger.date, Messenger.prcp).all()
+
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of precipitation
+    precipitation_data = []
+    for date, precipitation in results:
+        precipitation_dict = {}
+        precipitation_dict["date"] = date
+        precipitation_dict["prcp"] = precipitation
+        precipitation_data.append(precipitation_dict)
+
+
+    return jsonify(all_names)
+
 
 @app.route("/api/v1.0/stations")
 def stations():
@@ -55,7 +80,7 @@ def stations():
 
 @app.route("/api/v1.0/tobs")
 def tobs():
-    """Return the tobs data as json"""
+    """Return the temperature observation data as json"""
 
     return jsonify(tobs_data)
 
